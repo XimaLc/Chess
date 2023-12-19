10e582539604b1568fde5878f3b6dc6e51b95764
XimaLc <74795630+XimaLc@users.noreply.github.com>
2023-12-19 13:58:52 +0100

    remove client

--- Chess/Chess/Source.cpp
+++ Chess/Chess/Source.cpp
@@ -3,19 +3,6 @@
 
 int main()
 {
-	//Game game;
-	Client client;
-
-	bool running = true;
-	int x;
-	while (running)
-	{
-		std::cout << "1 pour envoyer; 0 pour quitter" << std::endl;
-		std::cin >> x;
-		std::cin.ignore();
-		if (x == 0)
-			running = false;
-		if(x == 1)
-			client.sendMessage("MESSAGE");
-	}
+	Game game;
+	return 0;
 }
