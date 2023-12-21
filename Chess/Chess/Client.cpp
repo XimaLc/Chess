d7549806ac054995eee9035fd6860d16321b8be6
Ximalc <74795630+XimaLc@users.noreply.github.com>
2023-12-22 00:29:34 +0100

    Client

--- Chess/Chess/Client.cpp
+++ Chess/Chess/Client.cpp
@@ -1,1 +1,38 @@
 #include "Client.h"
+
+std::string Client::receiveMessage()
+{
+	char in[128];
+	in[127] = 1;
+	std::size_t received;
+	sf::IpAddress sender;
+
+	if (socket.receive(in, sizeof(in), received, sender, port) != sf::Socket::Status::Done)
+		return "0";
+
+	return in;
+}
+
+int Client::sendMessage(std::string _message)
+{
+	if (socket.send(_message.c_str(), sizeof(_message), ip, port) != sf::Socket::Status::Done)
+		return 0;
+
+	std::cout << "Message envoye au serveur" << std::endl;
+}
+
+std::string Client::waitForInfo()
+{
+	std::string res = "0";
+	while (res == "0")
+	{
+		res = receiveMessage();
+	}
+	return res;
+}
+
+Client::Client()
+{
+	ip = "127.0.0.1";
+	port = 8888;
+}
--- Chess/Chess/Client.h
+++ Chess/Chess/Client.h
@@ -1,5 +1,21 @@
 #pragma once
-class Client
-{
-};
+#include "SFML/Network.hpp"
+#include "Player.h"
+#include <iostream>
 
+class Client : public Player
+{
+public:
+	sf::IpAddress ip;
+	unsigned short port;
+
+	sf::UdpSocket socket;
+	std::string messageOut;
+	sf::IpAddress oppenent;
+
+	//void getOppenent();
+	int sendMessage(std::string _message);
+	std::string receiveMessage();
+	std::string waitForInfo();
+	Client();
+};
